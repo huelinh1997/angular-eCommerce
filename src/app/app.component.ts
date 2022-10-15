07b3a7963aff48f082444a7cc3229373bedb9d99
Linh Nguyen Hue <linhnh@tech.est-rouge.com>
2022-10-15 14:32:02 +0700

    [AGL] init source

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
+  // we add a decorator called Component, this component decorator add metadata
+  // to tell to the angular what is the role of this app component
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'angular-practice-ecommerce';
